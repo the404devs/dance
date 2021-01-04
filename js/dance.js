c557a37f61805ee5f4d4604b10112bbdab234c51
Owen Bowden <the404devs@gmail.com>
2021-01-04 18:30:41 -0500

    question mark fix

--- js/dance.js
+++ js/dance.js
@@ -4,6 +4,7 @@ function dance() {
     const phrase = $("#input").val().toLowerCase();
     phrase.split('').forEach(letter => {
         if ("abcdefghijklmnopqrstuvwxyz0123456789!?@$&".includes(letter)) {
+            if (letter == "?") { letter = "qm" }
             $("#fun-zone").append($("<img>").addClass("letter").attr("src", "img/" + letter + ".gif"));
         }
     });
