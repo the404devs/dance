be0fb4569ce2000859c5a9b5dbb07c88f5d834f0
Owen Bowden <the404devs@gmail.com>
2021-01-04 15:16:03 -0500

    0.0.1: Initial version, it works.

---
+++ js/dance.js
@@ -0,0 +1,11 @@
+function dance() {
+    // $("#fun-zone").fadeOut();
+    $("#fun-zone").children('.letter').remove();
+    const phrase = $("#input").val().toLowerCase();
+    phrase.split('').forEach(letter => {
+        if ("abcdefghijklmnopqrstuvwxyz0123456789!?@$&".includes(letter)) {
+            $("#fun-zone").append($("<img>").addClass("letter").attr("src", "img/" + letter + ".gif"));
+        }
+    });
+    $("#fun-zone").fadeIn();
+}
