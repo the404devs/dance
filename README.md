b4e91630cb1a2c36e8a21c4af627f107e47ee96c
Owen Bowden <the404devs@gmail.com>
2021-01-12 21:27:03 -0500

    0.0.2: Test for empty string, add explanation text

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ https://dance.the404.nl
 
 dancing letter gifs, wooo
 
+### *0.0.2 (01/12/21)*
+----------------------
+- Add a bit more words, so a user can understand what to do.
+- Refuse to generate gifs when the input string is just whitespace.
+- Made the input box a little less obnoxiously large.
+
 ### *0.0.1 (01/04/21)*
 ----------------------
 - Initial version
--- js/dance.js
+++ js/dance.js
@@ -2,6 +2,11 @@ function dance() {
     // $("#fun-zone").fadeOut();
     $("#fun-zone").children('.letter').remove();
     const phrase = $("#input").val().toLowerCase();
+    if (phrase.trim() == "") {
+        alert("Please put some actual text as input!");
+        $("#fun-zone").fadeOut();
+        return;
+    }
     phrase.split('').forEach(letter => {
         if ("abcdefghijklmnopqrstuvwxyz0123456789!?@$&".includes(letter)) {
             if (letter == "?") { letter = "qm" }
